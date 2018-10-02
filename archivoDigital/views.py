876b911f6061b1c219d4251f98266126e6de194b
sckrs <ing.sc.husg@gmail.com>
2018-10-02 15:22:10 -0500

    pagina con template de login integrado

--- archivoDigital/views.py
+++ archivoDigital/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+def userLogin(request):
+        if request.method == 'POST':
+            # First get the username and password supplied
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            # Django's built-in authentication function:
+            user = authenticate(username=username, password=password)
+
+            # If we have a user
+            if user:
+                #Check it the account is active
+                if user.is_active:
+                    # Log the user in.
+                    login(request,user)
+                    # Send the user back to some page.
+                    # In this case their homepage.
+                    return archivoDigital(request,username)
+                else:
+                    # If account is not active:
+                    return HttpResponse("Your account is not active.")
+            else:
+                print("Someone tried to login and failed.")
+                print("They used username: {} and password: {}".format(username,password))
+                return HttpResponse("Invalid login details supplied.")
+
+        else:
+            #Nothing has been provided for username or password.
+            return render(request, 'login_templates/login.html', {})
